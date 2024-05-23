9b7261435286e0a7313cbdf66bc5ad4b6511c3a5
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-05-23 14:14:52 +0900

    Feat: 무한 맵 이동 기능 추가

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
